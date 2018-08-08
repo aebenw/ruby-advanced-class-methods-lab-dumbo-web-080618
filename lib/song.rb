7a8f3d16b3ede2b51b90e5c655bd443e13060d40
Andrew Woodward <ebenwoodward@icloud.com>
2018-08-08 23:47:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,7 @@ class Song
   
   def self.find_or_create_by_name(title)
      if self.find_by_name(title) 
-        self.create_by_name(title)
+        self.name
       else 
         self.find_by_name(title)
      end
